2a90996a27fbed80c6fc685f5a6c4b1b8a2117e9
ekahnny <ekahnny@gmail.com>
2024-06-06 11:57:24 +0000

    menampilkan todo list pada component

--- todo-list/src/components/InputToDo.jsx
+++ todo-list/src/components/InputToDo.jsx
@@ -1,5 +1,5 @@
 const InputToDo = (props) => {
-    const {name, type, placeholder, onChange} = props
+    const {name, type, placeholder, value, onChange} = props
 
     return (
         <>
@@ -8,6 +8,7 @@ const InputToDo = (props) => {
                 className="mt-7 ml-3 text-sm border rounded w-5/6 h-10 py-2 px-3 text-slate-700 placeholder:opacity-80" 
                 type={type}
                 placeholder={placeholder}
+                value={value}
                 onChange={onChange}
             />
         </>
--- todo-list/src/pages/todo.jsx
+++ todo-list/src/pages/todo.jsx
@@ -4,24 +4,44 @@ import { useState } from "react"
 
 
 const ToDoPage = () => {
-    const [todo, setTodo] = useState("")
+    const [todo, setTodo] = useState({tasks: [], currentTask: ""})
     
+    // memperbaharui currentTask dalam state todo dengan inputan user
     const handleChangeToDo = (event) => {
-        const {name, value} = event.target
+        const {value} = event.target
         setTodo((prevToDo) => {
+            console.log("previous to do: ", prevToDo)
+            console.log("inputan user: ", event.target.value)
             return {
+                // menyalin semua property yg ada dalam todo
                 ...prevToDo, 
-                [name]: value,
+                // mengubah nilai currentTask dengan inputan user
+                currentTask: value,
             }
         })
      }
 
+    // menambahkan currentTask kedalam array tasks pada state todo
     const handleSubmitToDo = (event) => {
         event.preventDefault()
-        event.target.todo.value = ""
-        console.log(todo)
-    }
 
+        // jika `currentTask` kosong, fungsi tidak melakukan apa-apa
+        if(todo.currentTask.trim() === "") return;
+
+        setTodo(prevTodo => ({
+
+            // menyalin semua tugas yang ada dalam array tasks dan menambahkan currentTask ke dalamnya
+
+            /* ...prevTodo.tasks mengambil tasks sebelumnya, 
+                agar ketika user menambahkan tasks baru, tasks
+                sebelumnya tidak tergantikan oleh tasks yang baru
+            */ 
+
+            tasks: [...prevTodo.tasks, prevTodo.currentTask ],
+            currentTask: ""
+        }))        
+        console.log("state todo: ", todo)
+    }
 
     return (
         <>
@@ -34,11 +54,15 @@ const ToDoPage = () => {
                             name="todo"
                             type="text"
                             placeholder="Add a new tasks..."
+                            // Setiap kali currentTask berubah, nilai input field juga akan diperbarui secara otomatis.
+                            value={todo.currentTask}
                             onChange={handleChangeToDo}
                         />
                         <button className="h-10 px-3 font-semibold text-xl rounded-md bg-rose-400 text-black mt-7 ml-3" type="submit">+</button>
                     </form>
-                    <ToDoList children={todo.todo}/>
+                    {todo.tasks.length > 0 && todo.tasks.map((task, index) => (
+                        <ToDoList key={index}>{task}</ToDoList>
+                    ))}
                 </div>
             </div>
         </>
