d33318aa0cf771f1d7d4d407cc4eee56bae67775
ekahnny <ekahnny@gmail.com>
2024-07-21 07:04:38 +0000

    menampilkan data dinamis

--- project-listing/src/components/fragments/Card.jsx
+++ project-listing/src/components/fragments/Card.jsx
@@ -1,13 +1,11 @@
-import Home from "../../assets/home.svg"
-
-function Card() {
+function Card({image, title, desc, price, rating, guests, bedrooms}) {
     return (
-      <div className="bg-dark-gray text-white p-8">
-        <div className="ml-14 mt-14 border border-white w-[357px] rounded-lg">
-            <img className="rounded-lg" src="https://csyxkpbavpcrhwqhcpyy.supabase.co/storage/v1/object/public/assets/property-challenge/thumbnail-1.jpg" alt="" />
+      <div className="bg-dark-gray text-white p-3">
+        <div className="ml-6 mt-4 border border-white w-[357px] rounded-lg">
+            <img className="rounded-lg" src={image} alt={title} />
             <div className="p-5">
-              <h1 className="font-bold">Nordic Retreat with Stunning Views</h1>
-              <p className="text-sm font-semibold mt-2 text-gray-500">Immerse yourself in the beauty of the Nordic landscape at this sleek and stylish cabin. With floor-to-ceiling windows and a spacious deck, you'll have breathtaking views of the surrounding mountains.</p>
+              <h1 className="font-bold">{title}</h1>
+              <p className="text-sm font-semibold mt-2 text-gray-500">{desc}</p>
               
               <div className="flex flex-row py-4">
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -16,20 +14,20 @@ function Card() {
                     <path d="M12.5 15H11.5C10.3954 15 9.5 15.8954 9.5 17V20.85C9.5 20.9328 9.56716 21 9.65 21H14.35C14.4328 21 14.5 20.9328 14.5 20.85V17C14.5 15.8954 13.6046 15 12.5 15Z" fill="#4A5567"/>
                     <rect x="16" y="5" width="2" height="4" rx="0.5" fill="#4A5567"/>
                 </svg>
-                <p className="text-xs my-1.5 ml-1">2 bedrooms</p>
+                <p className="text-xs my-1.5 ml-1">{bedrooms} bedrooms</p>
 
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-3">
                     <circle cx="12" cy="8" r="4" fill="#4A5567"/>
                     <path d="M5.33788 17.3206C5.99897 14.5269 8.77173 13 11.6426 13H12.3574C15.2283 13 18.001 14.5269 18.6621 17.3206C18.79 17.8611 18.8917 18.4268 18.9489 19.0016C19.0036 19.5512 18.5523 20 18 20H6C5.44772 20 4.99642 19.5512 5.0511 19.0016C5.1083 18.4268 5.20997 17.8611 5.33788 17.3206Z" fill="#7E869E" fill-opacity="0.25"/>
                 </svg>
-                <p className="text-xs my-1.5 ml-1">3 guests</p>
+                <p className="text-xs my-1.5 ml-1">{guests} guests</p>
               </div>
 
               <hr className="bg-slate-700" />
 
               <div className="flex justify-between my-3">
                   <div className="flex items-baseline">
-                      <p className="font-bold text-xl">$250/</p>
+                      <p className="font-bold text-xl">${price}/</p>
                       <p className="mt-1.5 text-sm">night</p>
                   </div>
 
@@ -37,7 +35,7 @@ function Card() {
                       <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M10.3072 7.21989C10.9493 5.61921 11.2704 4.81886 11.7919 4.70794C11.9291 4.67877 12.0708 4.67877 12.208 4.70794C12.7295 4.81886 13.0506 5.61921 13.6927 7.21989C14.0578 8.13017 14.2404 8.58531 14.582 8.89488C14.6778 8.98171 14.7818 9.05904 14.8926 9.12579C15.2874 9.36377 15.7803 9.40791 16.7661 9.4962C18.4348 9.64565 19.2692 9.72037 19.524 10.1961C19.5768 10.2946 19.6127 10.4013 19.6302 10.5117C19.7146 11.0447 19.1012 11.6028 17.8744 12.7189L17.5338 13.0289C16.9602 13.5507 16.6735 13.8116 16.5076 14.1372C16.4081 14.3325 16.3414 14.5428 16.3101 14.7598C16.258 15.1215 16.342 15.5 16.5099 16.257L16.5699 16.5274C16.8711 17.885 17.0217 18.5637 16.8337 18.8974C16.6649 19.1971 16.3538 19.3889 16.0102 19.4053C15.6277 19.4236 15.0887 18.9844 14.0107 18.106C13.3005 17.5273 12.9454 17.2379 12.5512 17.1249C12.191 17.0216 11.8089 17.0216 11.4487 17.1249C11.0545 17.2379 10.6994 17.5273 9.98917 18.106C8.91119 18.9844 8.37221 19.4236 7.98968 19.4053C7.64608 19.3889 7.33504 19.1971 7.16617 18.8974C6.97818 18.5637 7.12878 17.885 7.42997 16.5274L7.48998 16.257C7.65794 15.5 7.74191 15.1215 7.6898 14.7598C7.65854 14.5428 7.59182 14.3325 7.49232 14.1372C7.32645 13.8116 7.03968 13.5507 6.46613 13.0289L6.12546 12.7189C4.89867 11.6028 4.28527 11.0447 4.36975 10.5117C4.38724 10.4013 4.42312 10.2946 4.47589 10.1961C4.73069 9.72037 5.56507 9.64565 7.23384 9.4962C8.21962 9.40791 8.71251 9.36377 9.10735 9.12579C9.2181 9.05904 9.32211 8.98171 9.41793 8.89488C9.75954 8.58531 9.94211 8.13017 10.3072 7.21989Z" fill="#F6C768" stroke="#F6C768" stroke-width="2"/>
                       </svg>
-                      <p className="mx-1 font-semibold text-sm mt-0.5">4.9</p>
+                      <p className="mx-1 font-semibold text-sm mt-0.5">{rating}</p>
                   </div>
               </div>
             </div>
--- project-listing/src/pages/property.jsx
+++ project-listing/src/pages/property.jsx
@@ -3,15 +3,18 @@ import Card from "../components/fragments/Card"
 import heroImg from "../assets/hero-image.jpg";
 import { useEffect } from "react";
 import getDataFromApi from "../services/property.service";
+import { useState } from "react";
 
 
 const PropertyPages = () => {
+
+    const [properties, setProperties] = useState([])
 
     useEffect(() => {
         const fetchData = async () => {
             try {
                 const data = await getDataFromApi()
-                console.log(data);
+                setProperties(data)
             }catch (error){
                 console.log("Error fetching data: ", error);
             }
@@ -27,20 +30,34 @@ const PropertyPages = () => {
         alt="Background"
         className="absolute top-0 left-0 w-full h-5/6 object-cover z-0"
       />
-      {/* Text moved here, below Filter */}
 
       {/* Filter Component */}
       <div className="relative z-10 flex flex-col justify-end h-5/6 ml-20">
         <div className="ml-20 pb-44">
-          <h1 className="text-6xl font-bold mb-5 text-black">Peace, nature, and dream</h1>
+          <h1 className="text-6xl font-bold mb-5 text-black">Peace, nature, and dream.</h1>
           <p className="text-2xl mb-8 text-black">Find and book a great experience.</p>
         </div>
         <Filter />
       </div>
       
+      <h1 className="text-2xl text-white font-semibold pl-32 mt-20">Over 200 stays.</h1>
+      
 
       {/* Card Component */}
-      <Card />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
+        {properties.map((property, index) => (
+          <Card
+            key={index}
+            image={property.image}
+            title={property.title}
+            desc={property.description}
+            price={property.price}
+            rating={property.rating}
+            guests={property.capacity.people}
+            bedrooms={property.capacity.bedroom}
+          />
+        ))}
+      </div>
 
 
     </div>
